73b5d065b3dc6303ee8c2153e78e7e6e13e28bd5
savinofiore <fiore35.web@gmail.com>
2025-04-02 20:17:17 +0200

    setup android emulator

--- servifino/macos/Flutter/GeneratedPluginRegistrant.swift
+++ servifino/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,7 +12,7 @@ import firebase_core
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
-  FLTFirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFunctionsPlugin"))
+  FirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FirebaseFunctionsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
 }
